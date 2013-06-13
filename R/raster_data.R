4073e143d8026f4b46cb62ef38e720e46867d232
Piotr Mankowski <pmankowski@partners.org>
2013-06-13 18:37:28 -0400

    Presentation work

--- R/raster_data.R
+++ R/raster_data.R
@@ -15,31 +15,29 @@ raster_data.default <- function(input_data, ...) {
 }
 
 raster_data.list <- function(input_list, ...) {
-  rd <- list(title=input_list$title, save_path=input_list$save_path, file_name=input_list$file_name, df=NULL, blocks=NULL)
+  rd <- list(title=input_list$title, save_path=input_list$save_path, file_name=input_list$file_name)
   # Break up by event type
 
   # Process Block Events
 
   # Process Linear Events
 
-  
-  rd$block <- Reduce(function(...) merge(..., all=T), lapply(input_list$block_events, process_block_event))   
-  rd$df <- Reduce(function(...) merge(..., all=T), lapply(input_list$linear_events, process_linear_event))
+  rd$single_timepoints <- Reduce(function(...) merge(..., all=T), lapply(input_list$single_timepoints, process_single_timepoint_event))   
+  rd$blocks <- Reduce(function(...) merge(..., all=T), lapply(input_list$block_events, process_block_event))   
+  rd$linear <- process_linear_event(input_list$linear_events[[1]])
+  #Reduce(function(...) merge(..., all=T), lapply(input_list$linear_events, process_linear_event))
+
+  rd
 }
 
 
 process_block_event <- function(event_info) {
-  if(event_info$class = "block_start_end") {
-    res <- process_full_block_event(event_info)
-  } else if (event_info$class = "linear") {
-    res <- process_linear_event
-  } else if (event_info$class = "block_single_time") {
-    res <- process_intantenous_event(event_info)
-  }
-  st <- do.call(c, lapply(event_info$times, function(x) { xts::.parseISO8601(x[1])$first.time }))
-  et <- do.call(c, lapply(event_info$times, function(x) { xts::.parseISO8601(x[2])$first.time }))
 
-  df <- data.frame(name=event_info$name, start_time=st, end_time=et, color=event_info$color, group=event_info$group)  
+  if(event_info$class == "start_end") {
+    df <- process_start_end_block_event(event_info)
+  } else if (event_info$class == "duration") {
+    df <- process_block_duration_event(event_info)
+  } 
 
   # Divide into parts that span two days, and those that don't
   non_spanning <- df[format(df$start_time, "%Y%m%d") == format(df$end_time, "%Y%m%d"),]
@@ -51,37 +49,62 @@ process_block_event <- function(event_info) {
   df <- rbind(non_spanning, first_parts, second_parts)
   df$day = as.Date(df$start_time, tz="EST")
 
-  df$start_time <- do.call(c, lapply(df$start_time, function(x) { xts::.parseISO8601(format(x, "0001-01-01T%H:%M:%S"))$first.time }))  
-  df$end_time <- do.call(c, lapply(df$end_time, function(x) { xts::.parseISO8601(format(x, "0001-01-01T%H:%M:%S"))$first.time }))  
+  df$day_s <- do.call(c, lapply(df$day, format_date_label))
+
+  df$start_time <- do.call(c, lapply(df$start_time, function(x) { read_iso_time(format(x, "0001-01-01T%H:%M:%S")) }))  
+  df$end_time <- do.call(c, lapply(df$end_time, function(x) { read_iso_time(format(x, "0001-01-01T%H:%M:%S")) }))  
  
   df
 } 
 
 
-process_full_block_event <- function(event_info) {
-  st <- do.call(c, lapply(event_info$times, function(x) { xts::.parseISO8601(x[1])$first.time }))
-  et <- do.call(c, lapply(event_info$times, function(x) { xts::.parseISO8601(x[2])$first.time }))
+process_start_end_block_event <- function(event_info) {
+  st <- do.call(c, lapply(event_info$times, function(x) { read_iso_time(x[1]) }))
+  et <- do.call(c, lapply(event_info$times, function(x) { read_iso_time(x[2]) }))
 
   df <- data.frame(name=event_info$name, start_time=st, end_time=et, color=event_info$color, group=event_info$group)  
-
-  # Divide into parts that span two days, and those that don't
-  non_spanning <- df[format(df$start_time, "%Y%m%d") == format(df$end_time, "%Y%m%d"),]
-  spanning <- df[format(df$start_time, "%Y%m%d") != format(df$end_time, "%Y%m%d"),]
-  
-  first_parts <- mdply(spanning, first_div)
-  second_parts <- mdply(spanning, second_div)
-
-  df <- rbind(non_spanning, first_parts, second_parts)
-  df$day = as.Date(df$start_time, tz="EST")
-
-  df$start_time <- do.call(c, lapply(df$start_time, function(x) { xts::.parseISO8601(format(x, "0001-01-01T%H:%M:%S"))$first.time }))  
-  df$end_time <- do.call(c, lapply(df$end_time, function(x) { xts::.parseISO8601(format(x, "0001-01-01T%H:%M:%S"))$first.time }))  
  
   df
-
-
 }
 
-process_intantenous_event <- function(event_info) {
-
+read_iso_time <- function(time_string) {
+  xts::.parseISO8601(time_string)$first.time
 }
+
+process_single_timepoint_event <- function(event_info) {
+  t <- do.call(c, lapply(event_info$times, read_iso_time ))    
+
+  df <- data.frame(name=event_info$name, time=t, color=event_info$color, group=event_info$group)
+
+  df$day = as.Date(df$time, tz="EST")
+
+  df$day_s <- do.call(c, lapply(df$day, format_date_label))
+  df$time <- do.call(c, lapply(df$time, function(x) { read_iso_time(format(x, "0001-01-01T%H:%M:%S")) }))  
+
+  df
+}
+
+
+process_linear_event <- function(event_info) {
+
+  linear_event_info <- list(name=event_info$name, y_range=event_info$y_range, color=event_info$color)
+
+  x_times <- do.call(c, lapply(event_info$times, function(x) {read_iso_time(x[1])}))
+  y_vals <- do.call(c, lapply(event_info$times, function(x) {as.numeric(x[2])}))
+
+  linear_event_info$plot_data <- data.frame(time=x_times, y_value=y_vals)
+
+  linear_event_info$plot_data$day <- as.Date(linear_event_info$plot_data$time, tz="EST")
+
+  do.call(c, lapply(linear_event_info$plot_data$day, format_date_label))
+
+  linear_event_info$plot_data$time <- do.call(c, lapply(linear_event_info$plot_data$time, function(x) { read_iso_time(format(x, "0001-01-01T%H:%M:%S")) }))  
+  
+  linear_event_info
+}
+
+
+format_date_label <- function(date) {
+  format(date, format="%a %m/%d")
+}
+
--- R/raster_plot.R
+++ R/raster_plot.R
@@ -5,19 +5,26 @@ library(plyr)
 library(scales)
 library(grid)
 
+source("/usr/local/htdocs/raster.plotter/R/raster_data.R")
 
-set_up_plot <- function(df, title) {
+set_up_plot <- function(rd) {
   # Initialize Plot
-  plot <- ggplot(data=df)
+  plot <- ggplot(rd$df)
 
   # Add Rasters for block events
-  plot <- plot + geom_rect(aes(NULL, NULL, xmin=start_time, xmax=end_time, fill=name, ymin=as.numeric(group), ymax=as.numeric(group)+1))
+  plot <- plot + geom_rect(aes(NULL, NULL, xmin=start_time, xmax=end_time, fill=name, ymin=(-20 * (group-1)), ymax=((-20 * (group-1)) + 20)), data=rd$blocks)
+
+  # Add single timepoints
+  #plot <- plot + geom_segment(aes(x=time, xend=time, y=-20 * (group-1), yend=((-20))), data=rd$single_timepoints)
+
+  # Add plots for linear data  
+  #plot <- plot + geom_line(aes(time, y_value), data=rd$linear$plot_data)
   
   # Get rid of excess margins for double-plotting
   plot <- plot + theme(panel.margin = unit(0, "npc"))
 
   # Set Title
-  plot <- plot + ggtitle(title) 
+  plot <- plot + ggtitle(rd$title) 
 
   # Set Up X Axis
   limits <- c(as.POSIXct(strptime("0001-01-01T00:00:00", "%Y-%m-%dT%H:%M:%S")), as.POSIXct(strptime("0001-01-02T00:00:00", "%Y-%m-%dT%H:%M:%S")))
@@ -28,26 +35,23 @@ set_up_plot <- function(df, title) {
   plot <- plot + scale_y_continuous(labels=NULL, breaks=NULL, name="")
 
   # Set Up Fill Colors
-  plot <- plot + scale_fill_manual(values=alpha(levels(df$color), 0.7))
+  plot <- plot + scale_fill_manual(values=alpha(levels(rd$blocks$color), 0.7))
 
   # Set Up Faceting by Day
-  plot <- plot + facet_grid(day_s ~ .)
+  plot <- plot + facet_grid(day_s ~ .) + theme(strip.text.y = element_text(angle=0)) 
 
 }
 
 
 plot_raster <- function(json_path) {
   # Read input and create data frame
-  json_input <- fromJSON(json_path)
-  df <- json_input.read(json_input)
-
-  # Set up day dates for correct faceting
-  df$day_s <- do.call(c, lapply(df$day, toString))
+  rd <- raster_data(fromJSON(json_path))
 
   # Initialize Plot
-  plot <- set_up_plot(df, json_input$title)
+  plot <- set_up_plot(rd)
 
   # Save Plot
-  ggsave(plot=plot, filename=json_input$filename, path=json_input$save_path, dpi=80, units="in", width=13, height=8)
+  #ggsave(plot=plot, filename=json_input$filename, path=json_input$save_path, dpi=80, units="in", width=13, height=8)
   
+  plot 
 }
