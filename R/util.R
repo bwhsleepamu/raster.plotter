76baf5eca40df31cd742682a443899d3287cdef7
Piotr Mankowski <pmankowski@partners.org>
2015-04-07 22:50:47 -0400

    Adding double-plot functionality

--- R/util.R
+++ R/util.R
@@ -18,3 +18,26 @@ split_day_spanning_blocks <- function(dt, t_cycle=T_CYCLE, epoch_length=EPOCH_LE
   
   rbindlist(list(first_division, second_division))
 }
+
+
+# Double-Plotting
+double_plot <- function(df, should_do_it) {
+  ### DANGER ###
+  # This function changes the date of the right double-plot, causing the actual dates to not match up with the times. 
+  # The correct date for a given set of times is (day + double_plot_pos)
+  ###
+  if(should_do_it) { 
+    r_df <- df
+    l_df <- df
+    
+    l_df$double_plot_pos <- 0
+    r_df$double_plot_pos <- 1
+    r_df$day <- r_df$day - 1
+    if(!is.null(r_df$day_s))
+      r_df$day_s <- format(r_df$day, format="%Y-%m-%d")
+    return(rbind(l_df, r_df))
+  } else {
+    df$double_plot_pos <- 0
+    df
+  }
+}
